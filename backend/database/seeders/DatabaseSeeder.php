f16538285b9a3613220ba5ab8aa902aa77b21779
Nikita Balashov <noWorkingEmail@mail.ru>
2023-11-16 08:32:33 +0500

    в сидере изменены данные тестового пользователя

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,9 +15,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password'=> bcrypt('Asd$123123123'),
+            'name' => 'Test@example.com',
+            'email' => 'Test@example.com',
+            'password'=> bcrypt('Test@example.com'),
         ]);
     }
 }
